74847878fdf577491e1d2c331f99994300f10080
hamza chana <hamzachana96@gmail.com>
2020-12-28 21:01:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyCkYWeuJrdMQSkm54esXM-nszB0Nqx45gg',
+      authDomain: 'fir-angular-auth-e648d.firebaseapp.com',
+      projectId: 'fir-angular-auth-e648d',
+      storageBucket: 'fir-angular-auth-e648d.appspot.com',
+      messagingSenderId: '761000405990',
+      appId: '1:761000405990:web:3be4640908838dfab88427',
+    }),
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
